0xbmtoan/browser-laptop

commit 2f15abd97efe6f0a440cd4d206e854b965cc95f0

support for tabs.create auditors @bbondy @darkdh

js/stores/windowStore.js
@@ -169,9 +169,18 @@ const newFrame = (frameOpts, openInForeground, insertionIndex, nextKey) => {
   }
   frameOpts = frameOpts.toJS ? frameOpts.toJS() : frameOpts
 
+  // handle tabs.create properties
+  insertionIndex = frameOpts.index || insertionIndex
+
+  if (frameOpts.active !== undefined) {
+    openInForeground = frameOpts.active
+  }
+
   if (openInForeground === undefined) {
     openInForeground = true
   }
+
+  // TODO(bridiver) - handle pinned property
 
   // evaluate the location
   frameOpts.location = frameOpts.location || newFrameUrl()
@@ -189,12 +198,14 @@ const newFrame = (frameOpts, openInForeground, insertionIndex, nextKey) => {
     }
   }
 
-  if (nextKey === undefined) {
-    nextKey = incrementNextKey()
+  let partitionNumber = frameOpts.partitionNumber
+  if (!partitionNumber && frameOpts.partition) {
+    partitionNumber = FrameStateUtil.getPartitionNumber(frameOpts.partition)
   }
+
   let nextPartitionNumber = 0
-  if (frameOpts.partitionNumber) {
-    nextPartitionNumber = frameOpts.partitionNumber
+  if (partitionNumber) {
+    nextPartitionNumber = partitionNumber
     if (currentPartitionNumber < nextPartitionNumber) {
       currentPartitionNumber = nextPartitionNumber
     }
@@ -220,6 +231,10 @@ const newFrame = (frameOpts, openInForeground, insertionIndex, nextKey) => {
   }
   if (FrameStateUtil.isFrameKeyPinned(frames, frameOpts.parentFrameKey)) {
     insertionIndex = 0
+  }
+
+  if (nextKey === undefined) {
+    nextKey = incrementNextKey()
   }
 
   windowState = windowState.merge(
@@ -644,11 +659,11 @@ const doAction = (action) => {
       if (!action.detail) {
         if (windowState.getIn('contextMenuDetail', 'type') === 'autofill' &&
             windowState.getIn('contextMenuDetail', 'tabId') === action.tabId) {
+          windowState = windowState.delete('contextMenuDetail')
           if (action.notify) {
             ipc.send('autofill-popup-hidden', action.tabId)
           }
         }
-        windowState = windowState.delete('contextMenuDetail')
       } else {
         windowState = windowState.set('contextMenuDetail', action.detail)
       }

app/browser/tabs.js
@@ -1,7 +1,8 @@
-const {app, extensions, webContents} = require('electron')
 const appActions = require('../../js/actions/appActions')
-const { makeImmutable } = require('../common/state/immutableUtil')
+const messages = require('../..//js/constants/messages')
 const tabState = require('../common/state/tabState')
+const {app, extensions, webContents} = require('electron')
+const { makeImmutable } = require('../common/state/immutableUtil')
 
 let currentWebContents = {}
 
@@ -31,9 +32,30 @@ const updateTab = (tabId) => {
 
 const api = {
   init: (state, action) => {
-    process.on('add-new-contents', (e, openerTab, newTab, disposition, userGesture) => {
+    process.on('add-new-contents', (e, source, newTab, disposition, size, userGesture) => {
       if (userGesture === false) {
         e.preventDefault()
+        return
+      }
+
+      let location = newTab.getURL()
+      if (!location || location === '') {
+        location = 'about:blank'
+      }
+
+      const frameOpts = {
+        location,
+        partition: newTab.session.partition,
+        guestInstanceId: newTab.guestInstanceId,
+        active: disposition !== 'background-tab'
+      }
+
+      if (disposition === 'new-window' || disposition === 'new-popup') {
+        const windowOpts = makeImmutable(size)
+        appActions.newWindow(makeImmutable(frameOpts), windowOpts)
+      } else {
+        let hostWebContents = source.hostWebContents || source
+        hostWebContents.send(messages.SHORTCUT_NEW_FRAME, location, { frameOpts })
       }
     })
     app.on('web-contents-created', function (event, tab) {

js/components/frame.js
@@ -9,7 +9,6 @@ const webviewActions = require('../actions/webviewActions')
 const appActions = require('../actions/appActions')
 const ImmutableComponent = require('./immutableComponent')
 const Immutable = require('immutable')
-const immutableUtil = require('../../app/common/state/immutableUtil')
 const cx = require('../lib/classSet')
 const siteUtil = require('../state/siteUtil')
 const FrameStateUtil = require('../state/frameStateUtil')
@@ -264,45 +263,49 @@ class Frame extends ImmutableComponent {
     let location = this.props.location
     newSrc = newSrc || this.props.src
 
+    if (isSourceAboutUrl(newSrc)) {
+      newSrc = getTargetAboutUrl(newSrc)
+    } else if (isTorrentViewerURL(newSrc)) {
+      newSrc = getTargetMagnetUrl(newSrc)
+    }
+
+    let guestInstanceId = null
+
     // Create the webview dynamically because React doesn't whitelist all
     // of the attributes we need
     let webviewAdded = false
-    let guestInstanceId = null
     if (this.shouldCreateWebview()) {
-      // only set the guestInstanceId if this is a new frame
-      if (this.webview == null) {
-        guestInstanceId = this.props.guestInstanceId
-      }
-      while (this.webviewContainer.firstChild) {
-        this.webviewContainer.removeChild(this.webviewContainer.firstChild)
-      }
-      // the webview tag is where the user's page is rendered (runs in its own process)
-      // @see http://electron.atom.io/docs/api/web-view-tag/
+      guestInstanceId = this.props.guestInstanceId
       this.webview = document.createElement('webview')
+      if (guestInstanceId) {
+        if (!this.webview.setGuestInstanceId(guestInstanceId)) {
+          console.error('could not set guestInstanceId ' + guestInstanceId)
+          guestInstanceId = null
+        }
+      } else {
+        let partition = FrameStateUtil.getPartition(this.frame)
+        ipc.sendSync(messages.INITIALIZE_PARTITION, partition)
+        this.webview.setAttribute('partition', partition)
+      }
+
       this.addEventListeners()
       if (cb) {
         this.runOnDomReady = cb
         let eventCallback = (e) => {
           this.webview.removeEventListener(e.type, eventCallback)
-          // handle deprectaed zoom level site settings
-          if (this.zoomLevel) {
-            this.webview.setZoomLevel(this.zoomLevel)
-          }
           this.runOnDomReady()
           delete this.runOnDomReady
         }
         this.webview.addEventListener('did-attach', eventCallback)
       }
-      let partition = FrameStateUtil.getPartition(this.frame)
-      ipc.sendSync(messages.INITIALIZE_PARTITION, partition)
-      this.webview.setAttribute('partition', partition)
-      if (guestInstanceId) {
-        if (!this.webview.setGuestInstanceId(guestInstanceId)) {
-          guestInstanceId = null
-        }
-      }
+
       webviewAdded = true
     }
+
+    if (!guestInstanceId || newSrc !== getTargetAboutUrl('about:blank')) {
+      this.webview.setAttribute('src', newSrc)
+    }
+
     this.webview.setAttribute('data-frame-key', this.props.frameKey)
 
     const parsedUrl = urlParse(location)
@@ -312,14 +315,6 @@ class Frame extends ImmutableComponent {
     const hack = siteHacks[parsedUrl.hostname]
     if (hack && hack.userAgent) {
       this.webview.setUserAgentOverride(hack.userAgent)
-    }
-
-    if (!guestInstanceId || newSrc !== 'about:blank') {
-      let webviewSrc
-      if (isSourceAboutUrl(newSrc)) webviewSrc = getTargetAboutUrl(newSrc)
-      else if (isTorrentViewerURL(newSrc)) webviewSrc = getTargetMagnetUrl(newSrc)
-      else webviewSrc = newSrc
-      this.webview.setAttribute('src', webviewSrc)
     }
 
     if (webviewAdded) {
@@ -673,32 +668,6 @@ class Frame extends ImmutableComponent {
     this.webview.addEventListener('mouseleave', (e) => {
       currentWindow.webContents.send(messages.DISABLE_SWIPE_GESTURE)
     })
-    // @see <a href="https://github.com/atom/electron/blob/master/docs/api/web-view-tag.md#event-new-window">new-window event</a>
-    this.webview.addEventListener('new-window', (e) => {
-      let guestInstanceId = e.options && e.options.guestInstanceId
-      let windowOpts = e.options && e.options.windowOptions || {}
-      windowOpts.parentWindowKey = currentWindow.id
-      windowOpts.disposition = e.disposition
-      let delayedLoadUrl = e.options && e.options.delayedLoadUrl
-
-      let frameOpts = {
-        location: e.url,
-        parentFrameKey: this.props.frameKey,
-        isPrivate: this.props.isPrivate,
-        partitionNumber: this.props.partitionNumber,
-        // use the delayed load url for the temporary title
-        delayedLoadUrl,
-        guestInstanceId
-      }
-
-      if (e.disposition === 'new-window' || e.disposition === 'new-popup') {
-        appActions.newWindow(frameOpts, windowOpts)
-      } else {
-        let openInForeground = this.props.prefOpenInForeground === true ||
-          e.disposition !== 'background-tab'
-        windowActions.newFrame(frameOpts, openInForeground)
-      }
-    })
     this.webview.addEventListener('did-attach', (e) => {
       let tabId = this.webview.getWebContents().getId()
       if (this.props.tabId !== tabId) {
@@ -712,20 +681,14 @@ class Frame extends ImmutableComponent {
       this.props.onCloseFrame(this.frame)
     })
     this.webview.addEventListener('page-favicon-updated', (e) => {
-      // TODO(Anthony): more general solution on muon to prevent weview event
-      // from emitting after tab closed
-      if (e.favicons && e.favicons.length > 0 && immutableUtil.isImmutable(this.frame)) {
+      if (e.favicons && e.favicons.length > 0) {
         imageUtil.getWorkingImageUrl(e.favicons[0], (imageFound) => {
           windowActions.setFavicon(this.frame, imageFound ? e.favicons[0] : null)
         })
       }
     })
     this.webview.addEventListener('page-title-updated', ({title}) => {
-      // TODO(Anthony): more general solution on muon to prevent weview event
-      // from emitting after tab closed
-      if (immutableUtil.isImmutable(this.frame)) {
-        windowActions.setFrameTitle(this.frame, title)
-      }
+      windowActions.setFrameTitle(this.frame, title)
     })
     this.webview.addEventListener('show-autofill-settings', (e) => {
       windowActions.newFrame({ location: 'about:autofill' }, true)
@@ -734,14 +697,7 @@ class Frame extends ImmutableComponent {
       contextMenus.onShowAutofillMenu(e.suggestions, e.rect, this.frame)
     })
     this.webview.addEventListener('hide-autofill-popup', (e) => {
-      // TODO(Anthony): more general solution on muon to prevent weview event
-      // from emitting after tab closed
-      if (immutableUtil.isImmutable(this.frame)) {
-        let webContents = this.webview.getWebContents()
-        if (webContents && webContents.isFocused()) {
-          windowActions.autofillPopupHidden(this.props.tabId)
-        }
-      }
+      windowActions.autofillPopupHidden(this.props.tabId)
     })
     this.webview.addEventListener('ipc-message', (e) => {
       let method = () => {}
@@ -1010,9 +966,7 @@ class Frame extends ImmutableComponent {
       }
     })
     this.webview.addEventListener('did-get-response-details', (details) => {
-      if (immutableUtil.isImmutable(this.frame)) {
-        windowActions.gotResponseDetails(this.frame.get('tabId'), details)
-      }
+      windowActions.gotResponseDetails(this.frame.get('tabId'), details)
     })
     // Handle zoom using Ctrl/Cmd and the mouse wheel.
     // this.webview.addEventListener('mousewheel', this.onMouseWheel.bind(this))

js/components/main.js
@@ -344,7 +344,7 @@ class Main extends ImmutableComponent {
         }
       }
       let openInForeground = getSetting(settings.SWITCH_TO_NEW_TABS) === true || options.openInForeground
-      const frameOpts = {
+      const frameOpts = options.frameOpts || {
         location: url,
         isPrivate: !!options.isPrivate,
         isPartitioned: !!options.isPartitioned,

js/dispatcher/appDispatcher.js
@@ -117,12 +117,18 @@ if (process.type === 'browser') {
 
     let queryInfo = payload.queryInfo || payload.frameProps || (payload.queryInfo = {})
     queryInfo = queryInfo.toJS ? queryInfo.toJS() : queryInfo
-    if (event.sender.hostWebContents) {
+    if (!event.sender.isDestroyed() && event.sender.hostWebContents) {
       // received from an extension
       // only extension messages will have a hostWebContents
-      let win = require('electron').BrowserWindow.fromWebContents(event.sender.hostWebContents)
+
       // default to the windowId of the hostWebContents
-      queryInfo.windowId = queryInfo.windowId || win.id
+      if (!queryInfo.windowId) {
+        let win = require('electron').BrowserWindow.fromWebContents(event.sender.hostWebContents)
+        if (!win) {
+          return
+        }
+        queryInfo.windowId = win.id
+      }
       // add queryInfo if we only had frameProps before
       payload.queryInfo = queryInfo
 

js/lib/appUrlUtil.js
@@ -66,13 +66,13 @@ module.exports.getGenDir = function (url) {
 
 module.exports.getBraveIndexPath = function (relateivePath = '') {
   return module.exports.fileUrl(
-      path.resolve(__dirname, '..', '..') + '/app/extensions/brave/' + relateivePath)
+      path.resolve(__dirname, '..', '..') + '/app/extensions/brave/' + relateivePath).replace('file://', 'chrome://brave')
 }
 
 module.exports.getBraveExtIndexHTML = function () {
   return process.env.NODE_ENV === 'development'
-    ? module.exports.getBraveIndexPath('index-dev.html').replace('file://', 'chrome://brave')
-    : module.exports.getBraveIndexPath('index.html').replace('file://', 'chrome://brave')
+    ? module.exports.getBraveIndexPath('index-dev.html')
+    : module.exports.getBraveIndexPath('index.html')
 }
 
 /**

js/state/frameStateUtil.js
@@ -207,6 +207,13 @@ function isAncestorFrameKey (frames, frame, parentFrameKey) {
   return isAncestorFrameKey(frames, parentFrame, parentFrameKey)
 }
 
+function getPartitionNumber (partition) {
+  console.log(partition)
+  const regex = /partition-(\d+)/
+  const matches = regex.exec(partition)
+  return matches && matches[0]
+}
+
 function isSessionPartition (partition) {
   return partition && partition.startsWith('persist:partition-')
 }
@@ -511,6 +518,7 @@ module.exports = {
   getFrameByDisplayIndex,
   getFrameByKey,
   getFrameByTabId,
+  getPartitionNumber,
   getActiveFrame,
   setActiveFrameDisplayIndex,
   setActiveFrameIndex,

commit d5421673a4ee01d78a1068cde5ded65e756e6758

Put back in handler for null/empty context menu detail. This is required in order to hide any ContextMenu object
Caused by https://github.com/brave/browser-laptop/commit/2f15abd97efe6f0a440cd4d206e854b965cc95f0#diff-733d30fbf34816322d3f25dd59c1eb99

Fixes https://github.com/brave/browser-laptop/issues/6236

Auditors: @bridiver, @darkdh

Test Plan:
1. Launch Brave and use the kabob / cheeseburger icon
2. Notice the context menu comes up
3. Click anywhere on the screen (outside of the context menu)
4. Notice context menu closes itself

js/stores/windowStore.js
@@ -658,9 +658,10 @@ const doAction = (action) => {
     case windowConstants.WINDOW_AUTOFILL_POPUP_HIDDEN:
     case windowConstants.WINDOW_SET_CONTEXT_MENU_DETAIL:
       if (!action.detail) {
+        windowState = windowState.delete('contextMenuDetail')
+
         if (windowState.getIn(['contextMenuDetail', 'type']) === 'autofill' &&
             windowState.getIn(['contextMenuDetail', 'tabId']) === action.tabId) {
-          windowState = windowState.delete('contextMenuDetail')
           if (action.notify) {
             ipc.send('autofill-popup-hidden', action.tabId)
           }
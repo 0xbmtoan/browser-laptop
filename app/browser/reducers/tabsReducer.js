0xbmtoan/browser-laptop

commit c9ebce536b0bbfbd4f74fb3262d9974a699cf822

Don’t close or change tabs in the renderer. The renderer should request the changes and respond to them when the browser process updates

app/browser/reducers/tabsReducer.js
@@ -13,11 +13,9 @@ const windowAction = require('../../../js/actions/windowActions.js')
 const {makeImmutable} = require('../../common/state/immutableUtil')
 const {getFlashResourceId} = require('../../../js/flash')
 const {l10nErrorText} = require('../../common/lib/httpUtil')
-const windows = require('../windows')
 const Immutable = require('immutable')
 const dragTypes = require('../../../js/constants/dragTypes')
 const {frameOptsFromFrame} = require('../../../js/state/frameStateUtil')
-const {BrowserWindow} = require('electron')
 
 const tabsReducer = (state, action, immutableAction) => {
   action = immutableAction || makeImmutable(action)
@@ -29,91 +27,132 @@ const tabsReducer = (state, action, immutableAction) => {
       state = tabState.maybeCreateTab(state, action)
       break
     case appConstants.APP_TAB_MOVED: {
-      const tabId = action.get('tabId')
-      const frameOpts = action.get('frameOpts')
-      const browserOpts = action.get('browserOpts') || new Immutable.Map()
-      const windowId = action.get('windowId') || -1
-      state = tabs.moveTo(state, tabId, frameOpts, browserOpts, windowId)
+      setImmediate(() => {
+        const tabId = action.get('tabId')
+        const frameOpts = action.get('frameOpts')
+        const browserOpts = action.get('browserOpts') || new Immutable.Map()
+        const windowId = action.get('windowId') || -1
+        tabs.moveTo(state, tabId, frameOpts, browserOpts, windowId)
+      })
       break
     }
     case appConstants.APP_CREATE_TAB_REQUESTED:
-      tabs.createTab(action)
-      break
-    case appConstants.APP_MAYBE_CREATE_TAB_REQUESTED:
-      state = tabs.maybeCreateTab(state, action)
+      if (!action.getIn(['createProperties', 'windowId'])) {
+        const senderWindowId = action.getIn(['senderWindowId'])
+        if (senderWindowId) {
+          action = action.setIn(['createProperties', 'windowId'], senderWindowId)
+        }
+      }
+
+      setImmediate(() => {
+        if (action.get('activateIfOpen')) {
+          tabs.maybeCreateTab(state, action)
+        } else {
+          tabs.createTab(action)
+        }
+      })
       break
     case appConstants.APP_TAB_UPDATED:
       state = tabState.maybeCreateTab(state, action)
       break
-    case appConstants.APP_ACTIVE_WEB_CONTENTS_CLOSED: {
-      const tabValue = tabState.getActiveTabValue(state, BrowserWindow.getActiveWindow().id)
-      if (tabValue) {
-        const tabId = tabValue.get('tabId')
-        if (tabs.isDevToolsFocused(tabId)) {
-          state = tabs.toggleDevTools(state, tabId)
-        } else {
-          state = tabs.closeTab(state, tabId, false)
+    case appConstants.APP_TAB_CLOSE_REQUESTED:
+      {
+        const tabId = tabState.resolveTabId(state, action.get('tabId'))
+        if (tabId === tabState.TAB_ID_NONE) {
+          break
         }
+
+        setImmediate(() => {
+          if (tabId) {
+            if (tabs.isDevToolsFocused(tabId)) {
+              tabs.toggleDevTools(tabId)
+            } else {
+              tabs.closeTab(tabId, action.get('forceClosePinned'))
+            }
+          }
+        })
       }
       break
-    }
-    case appConstants.APP_TAB_CLOSED: {
-      const tabId = action.getIn(['tabValue', 'tabId'])
-      const forceClose = action.get('forceClose')
-      if (tabId) {
-        state = tabs.closeTab(state, tabId, forceClose)
+    case appConstants.APP_TAB_CLOSED:
+      {
+        const tabId = action.get('tabId')
+        if (tabId === tabState.TAB_ID_NONE) {
+          break
+        }
+        state = tabState.removeTabByTabId(state, tabId)
       }
       break
-    }
     case appConstants.APP_ALLOW_FLASH_ONCE:
     case appConstants.APP_ALLOW_FLASH_ALWAYS:
-      {
+      setImmediate(() => {
         const webContents = getWebContents(action.get('tabId'))
         if (webContents && !webContents.isDestroyed() && webContents.getURL() === action.get('url')) {
           webContents.authorizePlugin(getFlashResourceId())
         }
-        break
-      }
+      })
+      break
     case appConstants.APP_TAB_CLONED:
-      state = tabs.clone(state, action)
+      setImmediate(() => {
+        tabs.clone(action)
+      })
       break
     case appConstants.APP_TAB_PINNED:
-      state = tabs.pin(state, action)
+      setImmediate(() => {
+        tabs.pin(state, action.get('tabId'), action.get('pinned'))
+      })
       break
     case windowConstants.WINDOW_SET_AUDIO_MUTED:
-      state = tabs.setAudioMuted(state, action)
+      setImmediate(() => {
+        tabs.setAudioMuted(action)
+      })
       break
     case windowConstants.WINDOW_SET_ALL_AUDIO_MUTED:
       action.get('frameList').forEach((frameProp) => {
-        state = tabs.setAudioMuted(state, frameProp)
+        setImmediate(() => {
+          tabs.setAudioMuted(frameProp)
+        })
       })
       break
-    case windowConstants.WINDOW_SET_ACTIVE_FRAME:
-      tabs.setActive(action.getIn(['frameProps', 'tabId']))
+    case appConstants.APP_TAB_ACTIVATE_REQUESTED:
+      setImmediate(() => {
+        tabs.setActive(action.get('tabId'))
+      })
       break
     case appConstants.APP_TAB_TOGGLE_DEV_TOOLS:
-      state = tabs.toggleDevTools(state, action.get('tabId'))
+      setImmediate(() => {
+        tabs.toggleDevTools(action.get('tabId'))
+      })
       break
     case appConstants.APP_LOAD_URL_REQUESTED:
-      state = tabs.loadURL(state, action)
+      setImmediate(() => {
+        tabs.loadURL(action)
+      })
       break
     case appConstants.APP_LOAD_URL_IN_ACTIVE_TAB_REQUESTED:
-      state = tabs.loadURLInActiveTab(state, action.get('windowId'), action.get('url'))
+      setImmediate(() => {
+        tabs.loadURLInActiveTab(state, action.get('windowId'), action.get('url'))
+      })
       break
     case appConstants.APP_ON_GO_BACK:
-      state = tabs.goBack(state, action)
+      setImmediate(() => {
+        tabs.goBack(action.get('tabId'))
+      })
       break
     case appConstants.APP_ON_GO_FORWARD:
-      state = tabs.goForward(state, action)
+      setImmediate(() => {
+        tabs.goForward(action.get('tabId'))
+      })
       break
     case appConstants.APP_ON_GO_TO_INDEX:
-      state = tabs.goToIndex(state, action)
+      setImmediate(() => {
+        tabs.goToIndex(action.get('tabId'), action.get('index'))
+      })
       break
     case appConstants.APP_ON_GO_BACK_LONG:
       {
         const history = tabs.getHistoryEntries(state, action)
         const tabValue = tabState.getByTabId(state, action.get('tabId'))
-        const windowId = windows.getActiveWindowId()
+        const windowId = tabValue.get('windowId')
 
         if (history !== null) {
           windowAction.onLongBackHistory(
@@ -131,7 +170,7 @@ const tabsReducer = (state, action, immutableAction) => {
       {
         const history = tabs.getHistoryEntries(state, action)
         const tabValue = tabState.getByTabId(state, action.get('tabId'))
-        const windowId = windows.getActiveWindowId()
+        const windowId = tabValue.get('windowId')
 
         if (history !== null) {
           windowAction.onLongForwardHistory(

app/browser/menu.js
@@ -75,7 +75,7 @@ const createFileSubmenu = () => {
       label: locale.translation('closeTab'),
       accelerator: 'CmdOrCtrl+W',
       click: function (item, focusedWindow) {
-        appActions.activeWebContentsClosed()
+        appActions.tabCloseRequested(tabState.TAB_ID_ACTIVE)
       }
     }, {
       // This should be disabled when no windows are active.
@@ -629,10 +629,10 @@ const doAction = (action) => {
         createMenu()
       })
       break
-    case appConstants.APP_TAB_CLOSED:
+    case appConstants.APP_TAB_CLOSE_REQUESTED:
       appDispatcher.waitFor([appStore.dispatchToken], () => {
         action = makeImmutable(action)
-        const tab = getByTabId(appStore.getState(), action.getIn(['tabValue', 'tabId']))
+        const tab = getByTabId(appStore.getState(), action.get('tabId'))
         if (tab && !tab.get('incognito') && tab.get('url') !== 'about:newtab') {
           if (tab.get('frame')) {
             lastClosedUrl = tab.get('url')

app/browser/reducers/sitesReducer.js
@@ -11,7 +11,6 @@ const siteUtil = require('../../../js/state/siteUtil')
 const syncActions = require('../../../js/actions/syncActions')
 const syncUtil = require('../../../js/state/syncUtil')
 const Immutable = require('immutable')
-const {makeImmutable} = require('../../common/state/immutableUtil')
 const settings = require('../../../js/constants/settings')
 const {getSetting} = require('../../../js/settings')
 const writeActions = require('../../../js/constants/sync/proto').actions
@@ -20,11 +19,10 @@ const syncEnabled = () => {
   return getSetting(settings.SYNC_ENABLED) === true
 }
 
-const sitesReducer = (state, action, emitChanges) => {
+const sitesReducer = (state, action, immutableAction) => {
   switch (action.actionType) {
     case appConstants.APP_ON_CLEAR_BROWSING_DATA:
-      action = makeImmutable(action)
-      if (action.getIn(['clearDataDetail', 'browserHistory'])) {
+      if (immutableAction.getIn(['clearDataDetail', 'browserHistory'])) {
         state = state.set('sites', siteUtil.clearHistory(state.get('sites')))
         filtering.clearHistory()
       }
@@ -99,28 +97,29 @@ const sitesReducer = (state, action, emitChanges) => {
         state = syncUtil.updateSiteCache(state, siteDetail)
       })
       break
-    case appConstants.APP_TAB_PINNED:
-      const tab = state.get('tabs').find((tab) => tab.get('tabId') === action.tabId)
-      if (!tab) {
-        console.warn('Trying to pin a tabId which does not exist:', action.tabId, 'tabs: ', state.get('tabs').toJS())
-        break
-      }
-      const sites = state.get('sites')
-      const siteDetail = siteUtil.getDetailFromTab(tab, siteTags.PINNED, sites)
-      if (action.pinned) {
-        state = state.set('sites', siteUtil.addSite(sites, siteDetail, siteTags.PINNED))
-      } else {
-        state = state.set('sites', siteUtil.removeSite(sites, siteDetail, siteTags.PINNED))
-      }
-      if (syncEnabled()) {
-        state = syncUtil.updateSiteCache(state, siteDetail)
+    case appConstants.APP_TAB_UPDATED:
+      if (immutableAction.getIn(['changeInfo', 'pinned']) != null) {
+        const pinned = immutableAction.getIn(['changeInfo', 'pinned'])
+        const tabId = immutableAction.getIn(['tabValue', 'tabId'])
+        const tab = state.get('tabs').find((tab) => tab.get('tabId') === tabId)
+        if (!tab) {
+          console.warn('Trying to pin a tabId which does not exist:', tabId, 'tabs: ', state.get('tabs').toJS())
+          break
+        }
+        const sites = state.get('sites')
+        const siteDetail = siteUtil.getDetailFromTab(tab, siteTags.PINNED, sites)
+        if (pinned) {
+          state = state.set('sites', siteUtil.addSite(sites, siteDetail, siteTags.PINNED))
+        } else {
+          state = state.set('sites', siteUtil.removeSite(sites, siteDetail, siteTags.PINNED))
+        }
+        if (syncEnabled()) {
+          state = syncUtil.updateSiteCache(state, siteDetail)
+        }
       }
       break
-
-    case appConstants.APP_MAYBE_CREATE_TAB_REQUESTED:
     case appConstants.APP_CREATE_TAB_REQUESTED: {
-      action = makeImmutable(action)
-      const createProperties = action.get('createProperties')
+      const createProperties = immutableAction.get('createProperties')
       if (createProperties.get('pinned')) {
         state = state.set('sites', siteUtil.addSite(state.get('sites'),
           siteUtil.getDetailFromCreateProperties(createProperties), siteTags.PINNED))

app/browser/reducers/windowsReducer.js
@@ -19,11 +19,12 @@ const windowsReducer = (state, action, immutableAction) => {
     case appConstants.APP_WINDOW_READY:
       windows.windowReady(action.get('windowId'))
       break
-    case appConstants.APP_TAB_PINNED:
-      setImmediate(() => {
-        // check after the state has been updated
-        windows.pinnedTabsChanged()
-      })
+    case appConstants.APP_TAB_UPDATED:
+      if (immutableAction.getIn(['changeInfo', 'pinned']) != null) {
+        setImmediate(() => {
+          windows.pinnedTabsChanged()
+        })
+      }
       break
     case appConstants.APP_CLOSE_WINDOW:
       state = windows.closeWindow(state, action)

app/browser/tabMessageBox.js
@@ -90,7 +90,7 @@ const tabMessageBox = {
 
   onTabClosed: (state, action) => {
     action = makeImmutable(action)
-    const tabId = action.getIn(['tabValue', 'tabId'])
+    const tabId = action.get('tabId')
     if (tabId) {
       // remove callback; call w/ defaults
       const cb = messageBoxCallbacks[tabId]

app/browser/tabs.js
@@ -9,7 +9,7 @@ const tabState = require('../common/state/tabState')
 const {app, BrowserWindow, extensions, session, ipcMain} = require('electron')
 const {makeImmutable} = require('../common/state/immutableUtil')
 const {getTargetAboutUrl, getSourceAboutUrl, isSourceAboutUrl, newFrameUrl, isTargetAboutUrl} = require('../../js/lib/appUrlUtil')
-const {isURL, getUrlFromInput, toPDFJSLocation, getDefaultFaviconUrl} = require('../../js/lib/urlutil')
+const {isURL, getUrlFromInput, toPDFJSLocation, getDefaultFaviconUrl, isHttpOrHttps} = require('../../js/lib/urlutil')
 const {isSessionPartition} = require('../../js/state/frameStateUtil')
 const {getOrigin} = require('../../js/state/siteUtil')
 const {getSetting} = require('../../js/settings')
@@ -54,10 +54,10 @@ const getTabValue = function (tabId) {
   }
 }
 
-const updateTab = (tabId) => {
+const updateTab = (tabId, changeInfo = {}) => {
   let tabValue = getTabValue(tabId)
   if (tabValue) {
-    appActions.tabUpdated(tabValue)
+    appActions.tabUpdated(tabValue, makeImmutable(changeInfo))
   }
 }
 
@@ -91,6 +91,14 @@ const needsPartitionAssigned = (createProperties) => {
     createProperties.isPartitioned ||
     createProperties.partitionNumber ||
     createProperties.partition
+}
+
+const isAutoDiscardable = (createProperties) => {
+  if (createProperties.pinned) {
+    return false
+  }
+
+  return isHttpOrHttps(createProperties.url)
 }
 
 // TODO(bridiver) - refactor this into an action
@@ -285,7 +293,7 @@ const api = {
       let newTabValue = getTabValue(newTab.getId())
 
       let index
-      if (newTabValue && parseInt(newTabValue.get('index')) > -1) {
+      if (parseInt(newTabValue.get('index')) > -1) {
         index = newTabValue.get('index')
       }
 
@@ -300,12 +308,13 @@ const api = {
       const frameOpts = {
         location,
         partition: newTab.session.partition,
-        openInForeground: newTab.active,
+        openInForeground: !!newTabValue.get('active'),
         guestInstanceId: newTab.guestInstanceId,
         isPinned: !!newTabValue.get('pinned'),
         openerTabId,
         disposition,
-        index
+        index,
+        unloaded: !!newTabValue.get('discarded')
       }
 
       if (disposition === 'new-window' || disposition === 'new-popup') {
@@ -320,11 +329,12 @@ const api = {
       updateTab(tabId)
     })
 
-    process.on('chrome-tabs-updated', (tabId) => {
-      updateTab(tabId)
+    process.on('chrome-tabs-updated', (tabId, changeInfo) => {
+      updateTab(tabId, changeInfo)
     })
 
-    process.on('chrome-tabs-removed', (tabId) => {
+    process.on('chrome-tabs-removed', (tabId, windowId) => {
+      appActions.tabClosed(tabId, windowId)
       cleanupWebContents(tabId)
     })
 
@@ -334,18 +344,6 @@ const api = {
       }
       let tabId = tab.getId()
 
-      tab.once('destroyed', cleanupWebContents.bind(null, tabId))
-      tab.once('crashed', cleanupWebContents.bind(null, tabId))
-      tab.once('close', cleanupWebContents.bind(null, tabId))
-
-      tab.on('page-favicon-updated', function (e, favicons) {
-        if (favicons && favicons.length > 0) {
-          // tab.setTabValues({
-          //   faviconUrl: favicons[0]
-          // })
-          // updateTabDebounce(tabId)
-        }
-      })
       tab.on('unresponsive', () => {
         console.log('unresponsive')
       })
@@ -363,10 +361,6 @@ const api = {
     process.on('on-tab-created', (tab, options) => {
       if (tab.isDestroyed()) {
         return
-      }
-
-      if (options.index !== undefined) {
-        tab.setTabIndex(options.index)
       }
 
       tab.once('did-attach', () => {
@@ -394,7 +388,7 @@ const api = {
     }
   },
 
-  toggleDevTools: (state, tabId) => {
+  toggleDevTools: (tabId) => {
     const tab = getWebContents(tabId)
     if (tab && !tab.isDestroyed()) {
       if (tab.isDevToolsOpened()) {
@@ -403,7 +397,6 @@ const api = {
         tab.openDevTools()
       }
     }
-    return state
   },
 
   setActive: (tabId) => {
@@ -415,7 +408,7 @@ const api = {
     })
   },
 
-  loadURL: (state, action) => {
+  loadURL: (action) => {
     action = makeImmutable(action)
     const tabId = action.get('tabId')
     const tab = getWebContents(tabId)
@@ -429,12 +422,11 @@ const api = {
           url,
           partition: tab.session.partition
         })
-        return state
+        return
       }
 
       tab.loadURL(url)
     }
-    return state
   },
 
   loadURLInActiveTab: (state, windowId, url) => {
@@ -442,7 +434,6 @@ const api = {
     if (tabValue) {
       api.loadURLInTab(state, tabValue.get('tabId'), url)
     }
-    return state
   },
 
   loadURLInTab: (state, tabId, url) => {
@@ -454,20 +445,17 @@ const api = {
     return state
   },
 
-  setAudioMuted: (state, action) => {
+  setAudioMuted: (action) => {
     action = makeImmutable(action)
     const muted = action.get('muted')
     const tabId = action.get('tabId')
     const tab = getWebContents(tabId)
     if (tab && !tab.isDestroyed()) {
       tab.setAudioMuted(muted)
-      const tabValue = getTabValue(tabId)
-      return tabState.updateTab(state, { tabValue })
     }
-    return state
   },
 
-  clone: (state, action) => {
+  clone: (action) => {
     action = makeImmutable(action)
     const tabId = action.get('tabId')
     let options = action.get('options') || Immutable.Map()
@@ -480,27 +468,21 @@ const api = {
       tab.clone(options.toJS(), (newTab) => {
       })
     }
-    return state
   },
 
-  pin: (state, action) => {
-    action = makeImmutable(action)
-    const tabId = action.get('tabId')
-    const pinned = action.get('pinned')
+  pin: (state, tabId, pinned) => {
     const tab = getWebContents(tabId)
     if (tab && !tab.isDestroyed()) {
       const url = tab.getURL()
       // For now we only support 1 tab pin per URL
       const alreadyPinnedTab = tabState.queryTab(state, { url, pinned: true, partition: tab.session.partition })
       if (pinned && alreadyPinnedTab) {
-        tab.close(tab)
-        return tabState.removeTabByTabId(state, tabId)
+        api.closeTab(tabId)
+        return
       }
 
       tab.setPinned(pinned)
-      state = tabState.updateTabValue(state, getTabValue(tabId))
     }
-    return state
   },
 
   isDevToolsFocused: (tabId) => {
@@ -511,31 +493,32 @@ const api = {
       tab.isDevToolsFocused()
   },
 
-  closeTab: (state, tabId, forceClose) => {
+  closeTab: (tabId, forceClosePinned = false) => {
     const tabValue = getTabValue(tabId)
     if (!tabValue) {
-      return state
+      return false
     }
     const tab = getWebContents(tabId)
     try {
       if (tab && !tab.isDestroyed()) {
-        if (forceClose) {
-          tab.forceClose()
+        if (tabValue.get('pinned') && !forceClosePinned) {
+          tab.hostWebContents.send(messages.SHORTCUT_NEXT_TAB)
+          return false
         } else {
-          tab.close(tab)
+          tab.forceClose()
         }
       }
     } catch (e) {
-      // ignore
+      return false
     }
-    return tabState.removeTab(state, tabValue)
+    return true
   },
 
   create: (createProperties, cb = null) => {
     setImmediate(() => {
       createProperties = makeImmutable(createProperties).toJS()
       const switchToNewTabsImmediately = getSetting(settings.SWITCH_TO_NEW_TABS) === true
-      if (switchToNewTabsImmediately) {
+      if (createProperties.active == null && switchToNewTabsImmediately) {
         createProperties.active = true
       }
       if (!createProperties.url) {
@@ -547,6 +530,10 @@ const api = {
         if (isSessionPartition(createProperties.partition)) {
           createProperties.parent_partition = ''
         }
+      }
+      if (!isAutoDiscardable(createProperties)) {
+        createProperties.discarded = false
+        createProperties.autoDiscardable = false
       }
       extensions.createTab(createProperties, (tab) => {
         cb && cb(tab)
@@ -588,7 +575,7 @@ const api = {
 
       const currentWindowId = tabValue && tabValue.get('windowId')
       if (windowId != null && currentWindowId === windowId) {
-        return state
+        return
       }
 
       if (windowId == null || windowId === -1) {
@@ -596,13 +583,13 @@ const api = {
         // a new window to be created.
         const windowTabCount = tabState.getTabsByWindowId(state, currentWindowId).size
         if (windowTabCount === 1) {
-          return state
+          return
         }
       }
 
       if (tabValue.get('pinned')) {
         // If the current tab is pinned, then don't allow to drag out
-        return state
+        return
       }
 
       tab.detach(() => {
@@ -613,7 +600,6 @@ const api = {
         }
       })
     }
-    return state
   },
 
   maybeCreateTab: (state, action) => {
@@ -623,7 +609,7 @@ const api = {
     createProperties = createProperties.set('url', url)
     const windowId = createProperties.get('windowId')
     if (!windowId) {
-      return state
+      return
     }
     const tabData = tabState.getMatchingTab(state, createProperties, windowId, url)
     if (tabData) {
@@ -631,34 +617,27 @@ const api = {
     } else {
       api.createTab(action)
     }
-    return state
   },
 
-  goBack: (state, action) => {
-    action = makeImmutable(action)
-    const tab = getWebContents(action.get('tabId'))
+  goBack: (tabId) => {
+    const tab = getWebContents(tabId)
     if (tab && !tab.isDestroyed()) {
       tab.goBack()
     }
-    return state
   },
 
-  goForward: (state, action) => {
-    action = makeImmutable(action)
-    const tab = getWebContents(action.get('tabId'))
+  goForward: (tabId) => {
+    const tab = getWebContents(tabId)
     if (tab && !tab.isDestroyed()) {
       tab.goForward()
     }
-    return state
   },
 
-  goToIndex: (state, action) => {
-    action = makeImmutable(action)
-    const tab = getWebContents(action.get('tabId'))
+  goToIndex: (tabId, index) => {
+    const tab = getWebContents(tabId)
     if (tab && !tab.isDestroyed()) {
-      tab.goToIndex(action.get('index'))
+      tab.goToIndex(index)
     }
-    return state
   },
 
   getHistoryEntries: (state, action) => {

app/browser/webContentsCache.js
@@ -2,14 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-const appActions = require('../../js/actions/appActions')
-
 const currentWebContents = {}
 
 const cleanupWebContents = (tabId) => {
   if (currentWebContents[tabId]) {
     delete currentWebContents[tabId]
-    appActions.tabClosed({ tabId })
   }
 }
 

app/browser/windows.js
@@ -104,7 +104,7 @@ const updatePinnedTabs = (win) => {
         tab.get('url') === site.get('location') &&
         (tab.get('partitionNumber') || 0) === (site.get('partitionNumber') || 0))
       if (tab) {
-        appActions.tabClosed(tab, true)
+        appActions.tabCloseRequested(tab.get('tabId'), true)
       }
       win.__alreadyPinnedSites = win.__alreadyPinnedSites.remove(site)
     })

commit 8a564e3f95f3ac9edcb5348fdec9f58ea2858dfd

Fix error with tab reducer; code it calls no longer returns state
Fixes issue introduced with https://github.com/brave/browser-laptop/commit/c9ebce536b0bbfbd4f74fb3262d9974a699cf822#diff-2be38b3d97f35cd818780bac4be979c4L589

Auditors: @bridiver

app/browser/reducers/tabsReducer.js
@@ -217,7 +217,7 @@ const tabsReducer = (state, action, immutableAction) => {
         const browserOpts = { positionByMouseCursor: true }
         frameOpts.indexByFrameKey = dragData.getIn(['dragOverData', 'draggingOverKey'])
         frameOpts.prependIndexByFrameKey = dragData.getIn(['dragOverData', 'draggingOverLeftHalf'])
-        state = tabs.moveTo(state, frame.get('tabId'), frameOpts, browserOpts, dragData.get('dropWindowId'))
+        tabs.moveTo(state, frame.get('tabId'), frameOpts, browserOpts, dragData.get('dropWindowId'))
       }
       break
     }